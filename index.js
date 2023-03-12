7d4164e12f47a69f6de48078b89a2dcc06989079
AzharTakoy <azharahmedtakoy4444@gmail.com>
2023-03-12 11:16:37 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-function addingEventListener() {
+const input= document.getElementById('button').innerHTML = 'Button is Clicked!';
+
+function addingEvenyByListener() {
+    
+  alert('I was clicked!');
 }
+
+input.addEventListener('click',addingEvenyByListener);
